b9e9a799639706b16ccaea9bc4070e602551c9b7
Juan Wagner <juanswagner@gmail.com>
2021-06-27 00:34:22 -0300

    b: Add envs for prod and dev

--- backend/src/config/GlobalVars.ts
+++ backend/src/config/GlobalVars.ts
@@ -1,8 +1,17 @@
+// Check environment
+const isProd = process.env.NODE_ENV === "production" ? "production" : "development"
+import { existsSync } from 'fs'
+import { resolve } from "path"
+const envPath = resolve(process.cwd(),
+  `.env.${isProd ? "production" : "development"}`
+)
+
 import { config } from "dotenv";
-config();
+// if dev or prod env file is available, take it. Other use lead std .env
+if (existsSync(envPath)) config({ path: envPath });
 
 export const GlobalVars = {
-  NODE_ENV: process.env.NODE_ENV === "production" ? process.env.NODE_ENV : "development",
+  isProd,
   PORT: parseInt(process.env.PORT as string) || 3000,
   db: {
     URL: process.env.MONGODB_URI,
