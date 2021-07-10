f72f15f14a990230d019480033dd19ddf728bf6b
Juan Wagner <juanswagner@gmail.com>
2021-07-10 18:18:37 -0300

    b: add mongo debug log

--- backend/src/config/globalVars.ts
+++ backend/src/config/globalVars.ts
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 import { existsSync } from 'fs'
 import { resolve } from "path"
 
-export const getEnvValueTruthy = (value: string): boolean => ["true", "1", "yes", "on"].includes(value.toLowerCase())
+export const getEnvValueTruthy =
+  (value: string): boolean => ["true", "1", "yes", "on"].includes(value.toLowerCase())
 
 /* LOAD CONFIGURATION */
 const isProd = process.env.NODE_ENV === "production"
@@ -20,6 +21,7 @@ export const GlobalVars = {
     DB: process.env.MONGODB_DB ?? "",
   },
   debug: {
-    showErrorTrace: getEnvValueTruthy(String(process.env.SHOW_ERROR_TRACE))
+    SHOW_ERROR_TRACE: getEnvValueTruthy(String(process.env.SHOW_ERROR_TRACE)),
+    SHOW_MONGO_ACTIONS: getEnvValueTruthy(String(process.env.SHOW_MONGO_ACTIONS)),
   }
 }
