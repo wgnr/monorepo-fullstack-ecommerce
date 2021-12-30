cf07aae53f046843c070cf23b3bc784aa618c49e
Juan Wagner <juanswagner@gmail.com>
2021-12-30 12:04:38 -0300

    chrore: change coverage folder path

--- jest.config.ts
+++ jest.config.ts
@@ -45,7 +45,7 @@ const config: Config.InitialOptions = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: "../coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/", ".spec.ts"],
